506466b6790a83bb582f4044224fdb01af90cacc
Juan C. Andreu <andreujuanc@gmail.com>
2021-10-10 00:42:28 +0200

    small improvements

--- app/src/pages/auctions/index.tsx
+++ app/src/pages/auctions/index.tsx
@@ -1,4 +1,5 @@
 import { BigNumber } from "@ethersproject/bignumber";
+import { formatUnits, parseUnits } from "@ethersproject/units";
 import { ethers } from "ethers";
 import { useCallback, useEffect, useState } from "react";
 import Button from "../../components/button";
@@ -32,8 +33,10 @@ export default function Auctions() {
     )
 }
 
+type CurrentAuction = Auction & { isSelf?: boolean }
+
 function AuctionItem(props: { auction: Auction }): JSX.Element {
-    const [auction, setAuction] = useState<Auction>(props.auction)
+    const [auction, setAuction] = useState<CurrentAuction>(props.auction)
     const contracts = useContracts()
     const signer = useSigner()
     const [bidAmount, setBidAmount] = useState<BigNumber>()
@@ -41,13 +44,14 @@ function AuctionItem(props: { auction: Auction }): JSX.Element {
     const [kaomoji, setKaomoji] = useState<KaoMojiItem>()
 
     const nowTimeStamp = BigNumber.from(Math.round((Date.now() / 1000)))
-    const endOfAuction = auction?.firstBidTime?.add(auction.duration)
+    const endOfAuction = auction?.firstBidTime?.eq(0) ? undefined : auction?.firstBidTime?.add(auction.duration)
     const started = auction?.firstBidTime?.eq(0) || (endOfAuction && nowTimeStamp.lt(endOfAuction))
     const ended = auction?.firstBidTime?.gt(0) && (endOfAuction && nowTimeStamp.gt(endOfAuction))
 
     const getAuction = useCallback(async () => {
         const auctionData = await contracts?.auction.auctions(props.auction.auctionId)
-        if (!auctionData) return
+        const account = await signer?.getAddress()
+        if (!account || !auctionData) return
 
         //const kaoFilter = contracts?.moji?.filters?.Transfer(ethers.constants.AddressZero, undefined, auctionData.tokenId)
         const tokenData = await contracts?.moji.getDataOf(auctionData.tokenId)
@@ -63,7 +67,8 @@ function AuctionItem(props: { auction: Auction }): JSX.Element {
             ...auction,
             amount: auctionData?.amount ?? BigNumber.from('0'),
             firstBidTime: auctionData?.firstBidTime ?? BigNumber.from('0'),
-
+            bidder: auctionData?.bidder,
+            isSelf: auctionData.bidder?.toLowerCase() == account.toLowerCase()
         })
 
     }, [auction, contracts, props.auction.auctionId])
@@ -108,14 +113,17 @@ function AuctionItem(props: { auction: Auction }): JSX.Element {
             <div>
                 Token Id {auction.tokenId?.toString()}
             </div>
-            {started && !ended && <div>
+            {started && !ended && endOfAuction && <div>
                 Ends in {endOfAuction && endOfAuction.sub(nowTimeStamp).div(60).toString() + 'min'}
+            </div>}
+            {started && !ended && !endOfAuction && <div>
+                Waiting for the first bid to start the auction!
             </div>}
             {ended && <div>
                 Auction Ended!
             </div>}
             <div>
-                Amount {auction.amount?.toString()}
+                Amount {auction?.amount && formatUnits(auction.amount, 18)}
             </div>
 
             <div>
@@ -125,21 +133,28 @@ function AuctionItem(props: { auction: Auction }): JSX.Element {
                 auction && started &&
                 <div>
                     <Input type={"number"}
-                        placeholder={auction.reservePrice?.toString()}
+                        placeholder={auction && formatUnits(auction?.reservePrice.toString(), 18)}
                         onChange={(x) => {
                             try {
                                 if (x === '')
                                     setBidAmount(undefined)
                                 else
-                                    setBidAmount(BigNumber.from(x))
+                                    setBidAmount(BigNumber.from(parseUnits(x, 18)))
                             } catch { }
                         }}
-                        value={bidAmount?.toString() ?? ""} />
+                        value={bidAmount ? formatUnits(bidAmount?.toString(), 18) : ""} />
                     {bidAmount && bidAmount?.gt(0) && allowance && allowance?.lt(bidAmount) && <Button onClick={increaseAllowance} >Increase allowance</Button>}
                     {
                         (!bidAmount || (bidAmount && allowance?.gte(bidAmount)))
                         && <Button onClick={placeBid} disabled={!bidAmount || !bidAmount?.gt(0)} >Place Bid</Button>
                     }
+                    <div style={{
+                        marginTop: '1rem',
+                        textAlign: 'center'
+                    }}>
+                        {auction?.isSelf === true && '(⌐■_■) You are the top bidder'}
+                        {auction?.isSelf === false && 'ಠ╭╮ಠ You are not the top bidder'}
+                    </div>
                 </div>
             }
             {ended && <Button onClick={claim} >Claim</Button>}
