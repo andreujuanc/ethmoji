917a0c798d9409841881f71837957cfaa9eaf31d
Juan C. Andreu <andreujuanc@gmail.com>
2021-10-30 01:54:11 +0200

    kaodao is upgreadable

--- contracts/KaoDao.sol
+++ contracts/KaoDao.sol
@@ -1,21 +1,33 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.2;
 
-import "@openzeppelin/contracts/governance/Governor.sol";
-import "@openzeppelin/contracts/governance/extensions/GovernorCountingSimple.sol";
-import "@openzeppelin/contracts/governance/extensions/GovernorVotes.sol";
-import "@openzeppelin/contracts/governance/extensions/GovernorVotesQuorumFraction.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts-upgradeable/governance/GovernorUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/governance/extensions/GovernorCountingSimpleUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/governance/extensions/GovernorVotesUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/governance/extensions/GovernorVotesQuorumFractionUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 
-contract KaoDao is Governor, GovernorCountingSimple, GovernorVotes, GovernorVotesQuorumFraction, AccessControl {
+/// @custom:security-contact me@something.com
+contract KaoDao is Initializable, 
+    GovernorUpgradeable,  GovernorCountingSimpleUpgradeable, GovernorVotesUpgradeable, GovernorVotesQuorumFractionUpgradeable,
+    UUPSUpgradeable, AccessControlUpgradeable {
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() initializer {}
+
     bytes32 public constant ADDRESS_UPDATER = keccak256("ADDRESS_UPDATER");
+    bytes32 public constant CONTRACT_UPGRADER = keccak256("CONTRACT_UPGRADER");
+    
     address private _kaoMoji;
 
-    constructor(ERC20Votes _token)
-        Governor("KaoDao")
-        GovernorVotes(_token)
-        GovernorVotesQuorumFraction(5)
+    function initialize(ERC20VotesUpgradeable _token) public initializer 
     {
+         __Governor_init("KaoDao");
+        __GovernorCountingSimple_init();
+        __GovernorVotes_init(_token);
+        __GovernorVotesQuorumFraction_init(25);
+        __UUPSUpgradeable_init();
         _setupRole(ADDRESS_UPDATER, msg.sender);
     }
 
@@ -33,12 +45,25 @@ contract KaoDao is Governor, GovernorCountingSimple, GovernorVotes, GovernorVote
         _kaoMoji = kaoMoji;
     }
 
-    // The following functions are overrides required by Solidity.
 
+    function propose(address[] memory targets, uint256[] memory values, bytes[] memory calldatas, string memory description)
+        public
+        override(GovernorUpgradeable)
+        returns (uint256)
+    {
+        require(targets.length == 1, "targets.length != 1");
+        require(_kaoMoji != address(0), "kaoMoji == 0");
+        require(targets[0] == _kaoMoji, "target != kaomoji");
+
+        return super.propose(targets, values, calldatas, description);
+    }
+
+
+    // The following functions are overrides required by Solidity.
     function quorum(uint256 blockNumber)
         public
         view
-        override(IGovernor, GovernorVotesQuorumFraction)
+        override(IGovernorUpgradeable, GovernorVotesQuorumFractionUpgradeable)
         returns (uint256)
     {
         return super.quorum(blockNumber);
@@ -47,29 +72,24 @@ contract KaoDao is Governor, GovernorCountingSimple, GovernorVotes, GovernorVote
     function getVotes(address account, uint256 blockNumber)
         public
         view
-        override(IGovernor, GovernorVotes)
+        override(IGovernorUpgradeable, GovernorVotesUpgradeable)
         returns (uint256)
     {
         return super.getVotes(account, blockNumber);
     }
 
-    function propose(address[] memory targets, uint256[] memory values, bytes[] memory calldatas, string memory description)
-        public
-        override(Governor)
-        returns (uint256)
-    {
-        require(targets.length == 1, "One proposal per transaction only");
-        require(_kaoMoji != address(0), "kaoMoji not set");
-        require(targets[0] == _kaoMoji, "target can only be the kaoMoji contract");
-
-        return super.propose(targets, values, calldatas, description);
-    }
 
     /**
      * @dev See {IERC165-supportsInterface}.
      */
-    function supportsInterface(bytes4 interfaceId) public view override(Governor, AccessControl) returns (bool) {
+    function supportsInterface(bytes4 interfaceId) public view override(GovernorUpgradeable, AccessControlUpgradeable) returns (bool) {
         return super.supportsInterface(interfaceId);
     }
+
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        onlyRole(CONTRACT_UPGRADER)
+        override
+    {}
 
 }
