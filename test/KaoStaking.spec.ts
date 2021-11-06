c44ea9abb1167e2fb9fe9982d838b0b85e57da8f
Juan C. Andreu <andreujuanc@gmail.com>
2021-11-06 11:23:36 +0000

    cleanup checkpoint

--- test/KaoStaking.spec.ts
+++ test/KaoStaking.spec.ts
@@ -12,11 +12,11 @@ describe("KapStaking", function () {
         expect(await core.kaoStaking.totalSupply()).to.be.equal(parseUnits(stakedTotalSupply, 'ether').toString())
     }
 
-    const stake = async (core: KaoContracts, user: SignerWithAddress, amount: BigNumberish, satakedByUser: BigNumberish, stakedTotalSupply: BigNumberish) => {
-        await expect(() => core.kaoToken.connect(user).approve(core.kaoStaking.address, amount)).to.changeTokenBalance(core.kaoToken, user, '0')
-        await expect(() => core.kaoStaking.connect(user).stake(amount)).to.changeTokenBalance(core.kaoToken, user, BigNumber.from(amount).mul(-1))
-        expect(await core.kaoStaking.totalSupply()).to.be.equal(stakedTotalSupply.toString())
-        //expect(await core.kaoStaking.balanceOf(user.address)).to.be(satakedByUser.toString())
+    const stake = async (core: KaoContracts, user: SignerWithAddress, amount: string, satakedByUser: string, stakedTotalSupply: string) => {
+        await expect(() => core.kaoToken.connect(user).approve(core.kaoStaking.address, parseUnits(amount, 'ether'))).to.changeTokenBalance(core.kaoToken, user, '0')
+        await expect(() => core.kaoStaking.connect(user).stake(parseUnits(amount, 'ether'))).to.changeTokenBalance(core.kaoToken, user, BigNumber.from(parseUnits(amount, 'ether')).mul(-1))
+        expect(await core.kaoStaking.totalSupply()).to.be.equal(parseUnits(stakedTotalSupply, 'ether').toString())
+        expect(await core.kaoStaking.balanceOf(user.address)).to.be.eq(parseUnits(satakedByUser, 'ether').toString())
     }
 
     const withdraw = async (core: KaoContracts, user: SignerWithAddress, amount: BigNumberish, stakedTotalSupply: BigNumberish) => {
@@ -33,8 +33,8 @@ describe("KapStaking", function () {
         await transfer(core, user1, '10', '0')
         await transfer(core, user2, '20', '0')
 
-        await stake(core, user1, parseUnits('10', 'ether'), parseUnits('10', 'ether'), parseUnits('10', 'ether'))
-        await stake(core, user2, parseUnits('20', 'ether'), parseUnits('20', 'ether'), parseUnits('30', 'ether'))
+        await stake(core, user1, '10', '10', '10')
+        await stake(core, user2, '20', '20', '30')
 
         await withdraw(core, user1, parseUnits('5', 'ether'), parseUnits('25', 'ether'))
         await withdraw(core, user2, parseUnits('10', 'ether'), parseUnits('15', 'ether'))
@@ -42,7 +42,7 @@ describe("KapStaking", function () {
         await withdraw(core, user1, parseUnits('5', 'ether'), parseUnits('10', 'ether'));
 
         await expect(withdraw(core, user2, parseUnits('1000', 'ether'), parseUnits('10', 'ether'))).to.be.revertedWith('ERC20: burn amount > balance')
-        await expect(stake(core, user2, parseUnits('1000', 'ether'), '0', parseUnits('10', 'ether'))).to.be.revertedWith('ERC20: transfer amount exceeds balance')
+        await expect(stake(core, user2, '1000', '0', '10')).to.be.revertedWith('ERC20: transfer amount exceeds balance')
 
 
     });
@@ -61,8 +61,8 @@ describe("KapStaking", function () {
         expect((await core.kaoStaking.getRewardsMultiplier(user2.address)).toString()).to.eq('0')
 
 
-        await stake(core, user1, parseUnits('100', 'ether'), '100', parseUnits('100', 'ether'))
-        await stake(core, user2, parseUnits('200', 'ether'), '200', parseUnits('300', 'ether'))
+        await stake(core, user1, '100', '100', '100')
+        await stake(core, user2, '200', '200', '300')
 
         expect((await core.kaoStaking.getRewardsMultiplier(user1.address)).toString()).to.eq('5') // 5% is the base fee
         expect((await core.kaoStaking.getRewardsMultiplier(user2.address)).toString()).to.eq('5') // 5% is the base fee
